dc93af0fb61d31b02988821b23d754019da31d70
Paul McKellar <paul.mckellar@gmail.com>
2017-12-14 14:00:09 -0800

    1

--- src/pow.cpp
+++ src/pow.cpp
@@ -100,7 +100,11 @@ bool CheckProofOfWork(uint256 hash, unsigned int nBits, const Consensus::Params&
     if (fNegative || bnTarget == 0 || fOverflow || bnTarget > UintToArith256(params.powLimit))
         return false;
 
-    LogPrintf("!!!!!!!!!!!!!! check range passed");
+    LogPrintf("!!!!!!!!!!!!!! check range passed\n");
+
+    LogPrintf("!!!!!!!!!!!!!! bnTarget %s\n", bnTarget.toString());
+    LogPrintf("!!!!!!!!!!!!!! UintToArith256(hash) %s\n", hash.toString());
+    LogPrintf("!!!!!!!!!!!!!! UintToArith256(hash) %s\n", UintToArith256(hash).toString());
 
 
     // Check proof of work matches claimed amount
